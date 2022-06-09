d335524db6876a69bef84bbd06ec95f68c6c40a1
TimoHong <h.chiuwing@gmail.com>
2022-06-09 17:31:34 +0800

    Initial commit

---
+++ src/test/java/com/TimoProject/SellApp/SellAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimoProject.SellApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
